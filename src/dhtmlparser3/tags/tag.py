aa46a4a675d53ea1c2b3e89d834a195445892fbb
Bystroushaak <bystrousak@kitakitsune.org>
2021-12-27 20:15:47 +0100

    Implemented .to_string() for Tag & __eq__ magic methods.

--- src/dhtmlparser3/tags/tag.py
+++ src/dhtmlparser3/tags/tag.py
@@ -1,3 +1,4 @@
+from dhtmlparser3.quoter import escape
 from dhtmlparser3.specialdict import SpecialDict
 
 
@@ -43,6 +44,41 @@ class Tag:
 
         return output
 
+    def to_string(self):
+        output = self.tag_to_str()
+        for item in self.content:
+            if isinstance(item, str):
+                output += item
+            else:
+                output += item.to_string()
+
+        if self.name:
+            return f"{output}</{self.name}>"
+
+        return output
+
+    def tag_to_str(self):
+        if not self.name:
+            return ""
+
+        if self.is_non_pair:
+            return f"<{self.name}{self._parameters_to_str()} />"
+
+        return f"<{self.name}{self._parameters_to_str()}>"
+
+    def _parameters_to_str(self):
+        if not self.parameters:
+            return ""
+
+        parameters = []
+        for key, value in self.parameters.items():
+            if value:
+                parameters.append(f"{key}={escape(value)}")
+            else:
+                parameters.append(f"{key}")
+
+        return " " + " ".join(parameters)
+
     def __repr__(self):
         parameters = (
             f"{repr(self.name)}",
@@ -51,3 +87,21 @@ class Tag:
         )
 
         return f"{self.__class__.__name__}({', '.join(parameters)})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Tag):
+            return False
+
+        if self.name != other.name:
+            return False
+
+        if self.parameters != other.parameters:
+            return False
+
+        if self.is_non_pair != other.is_non_pair:
+            return False
+
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
